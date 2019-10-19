859e4b2c67227d9eb0fc1c07df6f3ddd9587fbe3
Chandrashekhar <chandrashekharjangra@gmail.com>
2019-10-20 04:25:36 +0530

    Added Transactions

--- UserApp/app/src/main/java/com/example/userapp/ui/slideshow/SlideshowViewModel.kt
+++ UserApp/app/src/main/java/com/example/userapp/ui/slideshow/SlideshowViewModel.kt
@@ -1,10 +1,13 @@
 package com.example.userapp.ui.slideshow
 
+import android.os.Bundle
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import com.example.userapp.R
 
 class SlideshowViewModel : ViewModel() {
+
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is slideshow Fragment"
